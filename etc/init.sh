e995428863ed35a296adbbb1fe46ce57652d56af
luchnck <luchnck@yandex.ru>
2016-03-17 05:28:19 +0000

    fixed init.sh str#8 -b 0.0.0.0:8080 for right connect

--- etc/init.sh
+++ etc/init.sh
@@ -5,5 +5,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
+sudo gunicorn -b 0.0.0.0:8080 -c /home/box/web/etc/gunicorn.conf hello:app &
 
