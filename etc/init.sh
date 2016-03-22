6cbb5edaed7dc67020515086b5054681d55b46ef
luchnck <luchnck@yandex.ru>
2016-03-22 07:29:39 +0000

    fixed log-level gunicorn in init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -1,9 +1,11 @@
+git config --global user.name "luchnck"
+git config --global user.email "luchnck@yandex.ru"
 sudo mv /home/box/teststep /home/box/web
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/gunicorn stop
 cd /home/box/web/
 sudo gunicorn -b 0.0.0.0:8080 -c /home/box/web/etc/gunicorn.conf hello:app &
-sudo gunicorn_django -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf  ask/ask/settings.py &
+sudo gunicorn_django -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf --log-level=1  ask/ask/settings.py &
