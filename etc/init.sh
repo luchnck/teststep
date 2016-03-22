6456e3fad7829af32be91cab5d9fcd5a4fff7b8c
luchnck <luchnck@yandex.ru>
2016-03-22 14:05:11 +0000

    go

--- etc/init.sh
+++ etc/init.sh
@@ -8,4 +8,4 @@ sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn stop
 cd /home/box/web/
 sudo gunicorn -b 0.0.0.0:8080 -c /home/box/web/etc/gunicorn.conf hello:app &
-sudo gunicorn_django -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf --access-log-file acc.log --err-log-file err.log  ask/ask/settings.py &
+sudo gunicorn -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf --access-logfile acc.log --error-logfile err.log  /home/box/web/ask/ask/settings.py &
