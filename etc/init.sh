9a65d15a59af66c73799840af86c2a58af6fdf62
luchnck <luchnck@yandex.ru>
2016-03-23 03:46:12 +0000

    fixed init.sh project must start from project directory

--- etc/init.sh
+++ etc/init.sh
@@ -8,4 +8,5 @@ sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn stop
 cd /home/box/web/
 sudo gunicorn -b 0.0.0.0:8080 -c /home/box/web/etc/gunicorn.conf hello:app &
-sudo gunicorn -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf --access-logfile acc.log --error-logfile err.log  /home/box/web/ask/ask/settings.py &
+cd ask
+sudo gunicorn -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf --access-logfile acc.log --error-logfile err.log wsgi:application &
