b703496ccaa8a4d7be8459af066fdb84963c3f13
luchnck <luchnck@yandex.ru>
2016-03-23 10:44:39 +0000

    finished! all workscd web!

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,5 +18,10 @@ from django.contrib import admin
 from qa import views
 
 urlpatterns = [
-    url(r'^.*$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^question/(?P<qId>\d+/$)', views.test),
+    url(r'^ask/$',views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$',views.test),
 ]
