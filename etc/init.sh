60934351a2659a3cc1186780e29ea36788a71797
luchnck <luchnck@yandex.ru>
2016-03-23 04:53:32 +0000

    logs moved to /tmp/, log-level ups to debug

--- etc/init.sh
+++ etc/init.sh
@@ -9,4 +9,4 @@ sudo /etc/init.d/gunicorn stop
 cd /home/box/web/
 sudo gunicorn -b 0.0.0.0:8080 -c /home/box/web/etc/gunicorn.conf hello:app &
 cd ask
-sudo gunicorn -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf --access-logfile acc.log --error-logfile err.log wsgi:application &
+sudo gunicorn -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf --log-level=debug --access-logfile /tmp/acc.log --error-logfile /tmp/err.log wsgi:application &
