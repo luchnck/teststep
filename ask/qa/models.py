b31d19556c7f9f14004e260192b2c63475d7e899
luchnck <luchnck@yandex.ru>
2016-03-28 05:06:15 +0000

    sqlBase added

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,3 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Question(models.model)
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.IntegerField()
+	author = models.CharField(max_length=255)
+	likes = models.ForeignKey(Likes, on_delete=models.CASCADE)
+	
+	def __unicode__(self)
+		return self.title
+
+	def get_absolute_url(self)
+		return 'question/%s/' % self.pk
+	
+	class Meta:
+		db_table = "questions"
+		ordering = ['-creation_date']
+
