916e462f10ec967286e688857e0b9af8e1e049dc
luchnck <luchnck@yandex.ru>
2016-03-28 08:49:22 +0000

    writed mysql.sh

--- etc/mysql.sh
+++ etc/mysql.sh
@@ -1,4 +1,9 @@
 cd /etc/init.d/
 sudo ./mysql start
+
 mysql -uroot -e "create database qa_service"
+mysql -uroot -e "CREATE USER 'enth'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'enth'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+
 cd /home/box/web/
