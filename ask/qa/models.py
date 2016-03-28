eff9b18e46784acaffc509329950e4f824901a4e
luchnck <luchnck@yandex.ru>
2016-03-28 09:18:17 +0000

    models.py fixed Model aside model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth.models, import User
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
-class Question(models.model):
+class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateTimeField()
@@ -13,33 +13,33 @@ class Question(models.model):
 	author = models.CharField(max_length=255)
 	likes = models.ForeignKey(Likes, on_delete = models.CASCADE)
 	
-	def __unicode__(self)
+	def __unicode__(self):
 		return self.title
 
-	def get_absolute_url(self)
+	def get_absolute_url(self):
 		return 'question/%s/' % self.pk
 	
 	class Meta:
 		db_table = "questions"
 		ordering = ['-added_at']
 
-class Answer(models.model):
+class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	question = models.ForeignKey(Question, on_delete = models.DO_NOTHING)
 	author = models.ForeignKey(User, on_delete = models.DO_NOTHING)
 	
-	def __unicode__(self)
+	def __unicode__(self):
 		return self.text
 
-	def get_absolute_url(self)
-		return "question/%s/#Answer%s" % self.question.pk self.pk
+	def get_absolute_url(self):
+		return "question/%s/#Answer%s" % self.question.pk, self.pk
 
 	class Meta:
 		db_table = "answers"
 		ordering = ['-added_at']
 
-class Likes(models.model):
+class Likes(models.Model):
 	question = ForeignKey(Question, on_delete = models.DO_NOTHING)
 	users = ForeignKey(User, on_delete = models.DO_NOTHING)
 
