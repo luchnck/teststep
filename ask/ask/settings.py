e959582a1bc4e1c226cd7df5922f931d6ad8abce
luchnck <luchnck@yandex.ru>
2016-04-01 09:12:56 +0000

    in process debug

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-			BASE_DIR + '/templates',
+			BASE_DIR + '/web/templates',
 		],
         'APP_DIRS': True,
         'OPTIONS': {
