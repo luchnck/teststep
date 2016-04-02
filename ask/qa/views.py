e6c82b3fd2f1ed820200ca420422e8e8dc3ff726
luchnck <luchnck@yandex.ru>
2016-04-02 23:19:47 +0300

    first task ended

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -6,13 +6,21 @@ from django.template import Context, Template
 
 from models import Question
 
+from django.core.paginator import Paginator
+
 def test(request, *args, **kwargs):
 	return HttpResponse('OK')
 
 def latest(request, *args, **kwargs):
-	questions = Question.objects.all().order_by('added_at')[:10]
+	questions = Question.objects.all().order_by('added_at')[:100]
+	paginator = Paginator(questions,10)
+	page = request.GET.get('page',1)
+	paginator.baseurl = '?page='
+	page = paginator.page(page)
 	c = {
-		"questions" : questions,
+		"paginator" : paginator,
+		"page" : page,
+		"questions" : page.object_list,
 		}
 	return render_to_response('latest.html', c)
 	
