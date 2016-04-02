c6aae82cf0c0564067b7067505c6e46588843529
root <root@localhost.localdomain>
2016-04-02 13:30:18 +0300

    move template to ask/qa/templates

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -11,8 +11,8 @@ def test(request, *args, **kwargs):
 
 def latest(request, *args, **kwargs):
 	questions = Question.objects.all().order_by('added_at')[:10]
-	c = Context({"questions" : questions})
-	return render_to_response('latest.html', c)
+	c = Context{"questions" : questions}
+	return render('latest.html', c)
 	
 
 # Create your views here.
